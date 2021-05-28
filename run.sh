9d95668976c79a645cfc5a93fc80878ec3acbbc1
weichweich <14820950+weichweich@users.noreply.github.com>
2021-05-28 10:36:20 +0200

    move spec file to .env

--- run.sh
+++ run.sh
@@ -8,12 +8,12 @@ source .env
 set +o allexport
 
 # TEARDOWN AND DELETE
-docker compose down
+docker compose down -v
 
 # Get relay chain spec, genesis wasm+head
 
-docker run $KILT_IMG export-genesis-state --chain=/node/dev-specs/kilt-parachain/peregrine-kilt.json --runtime=$KILT_RUNTIME > specs/kilt-genesis.hex
-docker run $KILT_IMG export-genesis-wasm --chain=/node/dev-specs/kilt-parachain/peregrine-kilt.json --runtime=$KILT_RUNTIME > specs/kilt.wasm
+docker run $KILT_IMG export-genesis-state --chain=$KILT_RAW_SPEC_FILE --runtime=$KILT_RUNTIME > specs/kilt-genesis.hex
+docker run $KILT_IMG export-genesis-wasm --chain=$KILT_RAW_SPEC_FILE --runtime=$KILT_RUNTIME > specs/kilt.wasm
 docker run --entrypoint cat $KILT_IMG /node/dev-specs/kilt-parachain/peregrine-relay.json > specs/polkadot.raw.json
 
 docker compose up -d
