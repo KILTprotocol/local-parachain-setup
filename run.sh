3f163dfe0ef3895d7cdcc2ff6ee9dd483af5065e
weichweich <14820950+weichweich@users.noreply.github.com>
2021-05-28 10:13:01 +0200

    configure runtime

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -x
+set -e
 
 # READ ENV FILE
 set -o allexport
@@ -11,8 +12,8 @@ docker compose down
 
 # Get relay chain spec, genesis wasm+head
 
-docker run $KILT_IMG export-genesis-state --chain=/node/dev-specs/kilt-parachain/peregrine-kilt.json --runtime=spiritnet > specs/kilt-genesis.hex
-docker run $KILT_IMG export-genesis-wasm --chain=/node/dev-specs/kilt-parachain/peregrine-kilt.json --runtime=spiritnet > specs/kilt.wasm
+docker run $KILT_IMG export-genesis-state --chain=/node/dev-specs/kilt-parachain/peregrine-kilt.json --runtime=$KILT_RUNTIME > specs/kilt-genesis.hex
+docker run $KILT_IMG export-genesis-wasm --chain=/node/dev-specs/kilt-parachain/peregrine-kilt.json --runtime=$KILT_RUNTIME > specs/kilt.wasm
 docker run --entrypoint cat $KILT_IMG /node/dev-specs/kilt-parachain/peregrine-relay.json > specs/polkadot.raw.json
 
 docker compose up -d
