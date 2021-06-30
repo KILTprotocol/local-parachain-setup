4052d88199474825451a13af788db9060ea6fdf9
Antonio Antonino <antonio@kilt.io>
2021-06-30 12:12:35 +0200

    fix: update runtimes and fix small port issue

--- run.sh
+++ run.sh
@@ -7,8 +7,11 @@ set -o allexport
 source .env
 set +o allexport
 
+# PROJECT_NAME = <user_id>-<current_directory_name>
+PROJECT_NAME=$USER-${PWD##*/}
+
 # TEARDOWN AND DELETE
-docker compose down -v
+docker compose -p $PROJECT_NAME down -v
 
 # Get relay chain spec, genesis wasm+head
 
@@ -16,4 +19,4 @@ docker run $KILT_IMG export-genesis-state --chain=$KILT_RAW_SPEC_FILE --runtime=
 docker run $KILT_IMG export-genesis-wasm --chain=$KILT_RAW_SPEC_FILE --runtime=$KILT_RUNTIME > specs/kilt.wasm
 docker run --entrypoint cat $KILT_IMG /node/dev-specs/kilt-parachain/peregrine-relay.json > specs/polkadot.raw.json
 
-docker compose up -d
+docker compose -p $PROJECT_NAME up -d
