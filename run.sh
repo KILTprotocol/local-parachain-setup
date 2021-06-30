cd749377ad1df2c65070504bb02602368fd5990c
Raphael Flechtner <39338561+rflechtner@users.noreply.github.com>
2021-06-30 14:10:07 +0200

    feat: easy customization of docker project name (#3)

--- run.sh
+++ run.sh
@@ -8,12 +8,12 @@ source .env
 set +o allexport
 
 # TEARDOWN AND DELETE
-docker compose down -v
+docker compose -p $DOCKER_PROJECT_NAME down -v
 
 # Get relay chain spec, genesis wasm+head
 
-docker run $KILT_IMG export-genesis-state --chain=$KILT_RAW_SPEC_FILE --runtime=$KILT_RUNTIME > specs/kilt-genesis.hex
-docker run $KILT_IMG export-genesis-wasm --chain=$KILT_RAW_SPEC_FILE --runtime=$KILT_RUNTIME > specs/kilt.wasm
-docker run --entrypoint cat $KILT_IMG /node/dev-specs/kilt-parachain/peregrine-relay.json > specs/polkadot.raw.json
+docker run --rm $KILT_IMG export-genesis-state --chain=$KILT_RAW_SPEC_FILE --runtime=$KILT_RUNTIME > specs/kilt-genesis.hex
+docker run --rm $KILT_IMG export-genesis-wasm --chain=$KILT_RAW_SPEC_FILE --runtime=$KILT_RUNTIME > specs/kilt.wasm
+docker run --rm --entrypoint cat $KILT_IMG /node/dev-specs/kilt-parachain/peregrine-relay.json > specs/polkadot.raw.json
 
-docker compose up -d
+docker compose -p $DOCKER_PROJECT_NAME up -d
