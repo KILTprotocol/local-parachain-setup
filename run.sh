6642fa75c305cf994bb719df60bf23095e3217dd
Marton Csernai <marton@kilt.io>
2021-07-13 17:20:28 +0200

    fix: merge leftovers from aw-local-db

--- run.sh
+++ run.sh
@@ -15,9 +15,14 @@ export USER_ID=$(id -u $USER)
 docker compose -p $PROJECT_NAME down -v
 
 # Get relay chain spec, genesis wasm+head
+echo "running with user id: " $USER_ID
 
 docker run --rm $KILT_IMG export-genesis-state --chain=$KILT_RAW_SPEC_FILE --runtime=$KILT_RUNTIME > specs/kilt-genesis.hex
 docker run --rm $KILT_IMG export-genesis-wasm --chain=$KILT_RAW_SPEC_FILE --runtime=$KILT_RUNTIME > specs/kilt.wasm
 docker run --rm --entrypoint cat $KILT_IMG /node/dev-specs/kilt-parachain/peregrine-relay.json > specs/polkadot.raw.json
+
+# Remove old data and create folders
+rm -rf ./volume/*
+mkdir -p ./volume/{relay-alice,relay-bob,relay-charlie,kilt-alice,kilt-bob}
 
 docker compose -p $PROJECT_NAME up -d
