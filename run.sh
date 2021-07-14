55ec6e8e73085c83e680ce1023a560b37405a1f1
weichweich <14820950+weichweich@users.noreply.github.com>
2021-07-14 10:18:29 +0200

    clean up

--- run.sh
+++ run.sh
@@ -14,15 +14,16 @@ export USER_ID=$(id -u $USER)
 # TEARDOWN AND DELETE
 docker compose -p $PROJECT_NAME down -v
 
-# Get relay chain spec, genesis wasm+head
-echo "running with user id: " $USER_ID
-
-docker run --rm $KILT_IMG export-genesis-state --chain=$KILT_RAW_SPEC_FILE --runtime=$KILT_RUNTIME > specs/kilt-genesis.hex
-docker run --rm $KILT_IMG export-genesis-wasm --chain=$KILT_RAW_SPEC_FILE --runtime=$KILT_RUNTIME > specs/kilt.wasm
-docker run --rm --entrypoint cat $KILT_IMG /node/dev-specs/kilt-parachain/peregrine-relay.json > specs/polkadot.raw.json
-
 # Remove old data and create folders
 rm -rf ./volume/*
-mkdir -p ./volume/{relay-alice,relay-bob,relay-charlie,kilt-alice,kilt-bob}
+mkdir -p ./volume/{relay-{alice,bob,charlie},kilt-{alice,bob}}
+
+echo "running with user id: " $USER_ID
+
+# Get relay chain spec, genesis wasm+head
+docker run --rm $KILT_IMG export-genesis-state --chain=$KILT_RAW_SPEC_FILE --runtime=$KILT_RUNTIME > specs/kilt-genesis.hex
+docker run --rm $KILT_IMG export-genesis-wasm --chain=$KILT_RAW_SPEC_FILE --runtime=$KILT_RUNTIME > specs/kilt.wasm
+docker run --rm --entrypoint cat $KILT_IMG $RELAY_SPEC_SOURCE > specs/polkadot.raw.json
+
 
 docker compose -p $PROJECT_NAME up -d
