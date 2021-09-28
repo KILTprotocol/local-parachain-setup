2572ae28eee97a8e50cf894f74c11dfc1f0e6b9b
William Freudenberger <william@kilt.io>
2021-09-28 09:57:21 +0200

    fix: spiritnet-dev setup (#8)
    * feat: upgrade parachain register script
    
    * fix: spiritnet-dev
    
    * fix: relay-spec for spiritnet-dev
    
    * fix: env

--- run.sh
+++ run.sh
@@ -16,7 +16,10 @@ docker compose -p $PROJECT_NAME down -v
 # Get relay chain spec, genesis wasm+head
 docker run --rm $KILT_IMG export-genesis-state --chain=$KILT_RAW_SPEC_FILE --runtime=$KILT_RUNTIME > specs/kilt-genesis.hex
 docker run --rm $KILT_IMG export-genesis-wasm --chain=$KILT_RAW_SPEC_FILE --runtime=$KILT_RUNTIME > specs/kilt.wasm
-docker run --rm --entrypoint cat $KILT_IMG /node/dev-specs/kilt-parachain/peregrine-stg-relay.json > specs/polkadot.raw.json
+
+# Active the line below if you are using a pre-compiled relay chain spec (peregrine {stg, prod})
+# Else you need to build your own relay spec in the Polkadot repository (rococo-local for dev)
+# docker run --rm --entrypoint cat $KILT_IMG /node/dev-specs/kilt-parachain/peregrine-stg-relay.json > specs/${RELAY_RAW_SPEC_FILE}
 
 # Spin it up the network and script
 docker compose -p $PROJECT_NAME up -d
