f61d3e32e249e400d1ecd9a2bc6dbfc0126208c4
William Freudenberger <william@kilt.io>
2022-08-31 11:23:09 +0200

    feat: update py script

--- scripts/update_para_id.py
+++ scripts/update_para_id.py
@@ -10,6 +10,13 @@ if __name__ == "__main__":
     with open(input_file, "r") as f:
         input_json = json.load(f)
 
+    if "raw" in input_json["genesis"] and input_json["para_id"] == new_paraid:
+        print("All good para ID is correct.")
+        sys.exit(0)
+    elif "raw" in input_json["genesis"]:
+        print("ERROR. Wrong paraID but raw spec can't be updated")
+        sys.exit(1)
+
     input_json["para_id"] = new_paraid
     input_json["genesis"]["runtime"]["parachainInfo"]["parachainId"] = new_paraid
     input_json["bootNodes"] = []
